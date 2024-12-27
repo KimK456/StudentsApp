be4aa29c5ca7fa26fbe7a11d270e36dd51ac1c39
Kim Kurikan <kim.kurikan@gmail.com>
2024-12-27 13:09:17 +0200

    added students list activity

---
+++ app/src/main/java/com/example/studentsapplication/adapter/StudentsRecyclerAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.studentsapplication.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.studentsapplication.OnItemClickListener
+import com.example.studentsapplication.R
+import com.example.studentsapplication.models.Student
+
+class StudentsRecyclerAdapter(private val students: List<Student>?): RecyclerView.Adapter<StudentViewHolder>() {
+
+    var listener: OnItemClickListener? = null
+
+    override fun getItemCount(): Int = students?.size ?: 0
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        val inflation = LayoutInflater.from(parent.context)
+        val view = inflation.inflate(R.layout.student_list_row, parent, false)
+        return StudentViewHolder(view, listener)
+    }
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        holder.bind(students?.get(position), position)
+    }
+}
